bca3a6d7ed12662322a739d475ae316b34a6aa72
AniketGhodake <ghodakeaniket27@gmail.com>
2023-09-27 23:16:37 +0530

    26 Sept 2023

--- Week 1/Debug1.cpp
+++ Week 1/Debug1.cpp
@@ -1,6 +1,7 @@
+/* Debug the code. Take input a character, print 1, if its a capital alphabet, 
+print 0, if its a lowercase alphabet, else print -1.*/
 #include<iostream>
 using namespace std;
-
 int main() {
 	char c;
     cin>>c;
@@ -11,6 +12,6 @@ int main() {
         cout<<"1";
     }
     else{
-        cout<<"-1";
+        cout<<"-1";                                                       // Debug Successfully
     }
 }
--- Week 1/Debug2.cpp
+++ Week 1/Debug2.cpp
@@ -1,7 +1,11 @@
+/* Debug the code. It is trying to print the given pattern.
+1
+23
+345
+4567 
+*/
 #include<iostream>
 using namespace std;
-
-
 int main(){
     int n;
     cin>>n;
@@ -10,7 +14,7 @@ int main(){
         
         int j = 1,count = i;
 
-        while(j<=i){
+        while(j<=i){                                                        // Debug Successfully
             cout<<count;           
             count = count + 1;
             j = j + 1;
